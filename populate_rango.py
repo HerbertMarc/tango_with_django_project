781c1037e452bbab2079f315a83e1c7f59a4369f
Marc Herbert <2250456h@student.gla.ac.uk>
2018-01-30 15:49:52 +0000

    should be finished 5

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,9 @@ def populate():
         {"title":"Flask",
         "url":"http://flask.pocoo.org"} ]
     
-    cats = {"Python": {"pages": python_pages},
-        "Django": {"pages": django_pages},
-        "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views" : 128 , "likes" : 64},
+        "Django": {"pages": django_pages, "views" : 64 , "likes" : 32},
+        "Other Frameworks": {"pages": other_pages, "views" : 32 , "likes" : 16} }
     
     # If you want to add more catergories or pages,
     # add them to the dictionaries above.
@@ -51,17 +51,18 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"],cat_data["views"],cat_data["likes"] )
     
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
     
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views, likes):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
+    p.likes=likes
     p.save()
     return p
 
--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page
+from django.utils.html import format_html
 
 # Register your models here.
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
+
+
